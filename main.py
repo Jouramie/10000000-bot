323d3bf0757a773c8650107d8d377750f3aa0303
jouramie <jouramie@hotmail.com>
2022-01-22 17:59:03 -0500

    complete refactoring

---
+++ main.py
@@ -0,0 +1,27 @@
+import pyautogui
+import logging
+import sys
+
+from PyQt6.QtWidgets import QApplication
+
+from src.domain.game_state import FetchGameState
+from src.ui.bot.game_state_observer import GameStateObserver
+from src.ui.overlay.overlay import Overlay
+
+logger = logging.getLogger(__name__)
+
+if __name__ == '__main__':
+
+    logger.info(f"Failsafe points {pyautogui.FAILSAFE_POINTS}")
+
+    app = QApplication(sys.argv)
+    displayRatio = app.devicePixelRatio()
+    logger.info(f"ratio = {displayRatio}")
+
+    fetch_game_state = FetchGameState()
+    main = Overlay()
+    game_state_observer = GameStateObserver(fetch_game_state, main.on_game_state_change, displayRatio)
+
+    game_state_observer.start()
+    main.showMaximized()
+    app.exec()
---
+++ src/domain/game_state.py
@@ -0,0 +1,24 @@
+import pyautogui
+
+from copy import deepcopy
+from typing import List
+
+def find_tiles():
+    return [
+        [tile.left, tile.top, tile.width, tile.height]
+        for tile in pyautogui.locateAllOnScreen('assets/tiles/sword.png') 
+    ] 
+
+
+class GameState:
+    def __init__(self) -> None:
+        self.tiles: List[List[int]] = []
+
+
+class FetchGameState:
+    def __init__(self) -> None:
+        self.game_state = GameState()
+
+    def execute(self) -> GameState:
+        self.game_state.tiles = find_tiles()
+        return deepcopy(self.game_state)
